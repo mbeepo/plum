9708c9fd5127e6c2a18263d4f0bd861a94639cab
Bee Clark <mrhumanman2@gmail.com>
2022-11-09 03:08:03 +0100

    unvec the interpret

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -99,163 +99,152 @@ impl Value {
     }
 }
 
-pub fn interpret<T: Into<Vec<Spanned>>>(input: T) -> Result<Vec<SpannedValue>, Vec<Error>> {
-    let input = input.into();
+pub fn interpret<T: AsRef<Spanned>>(input: T) -> Result<SpannedValue, Vec<Error>> {
+    let input = input.as_ref();
     let mut errors: Vec<Error> = Vec::new();
-    let mut results: Vec<SpannedValue> = Vec::new();
 
-    for item in input {
-        let out = match item {
-            Spanned(Expr::Literal(literal), span) => match literal {
-                Literal::Array(e) => {
-                    let mut errored = false;
+    match input {
+        Spanned(Expr::Literal(literal), span) => match literal {
+            Literal::Array(e) => {
+                let mut errored = false;
 
-                    let new = e
-                        .iter()
-                        .map(|f| {
-                            let interpreted = interpret(f.clone());
+                let new = e
+                    .iter()
+                    .map(|f| {
+                        let interpreted = interpret(f.clone());
 
-                            match interpreted {
-                                Ok(e) => e,
-                                Err(e) => {
-                                    errors.extend(e);
-                                    errored = true;
+                        match interpreted {
+                            Ok(e) => e,
+                            Err(e) => {
+                                errors.extend(e);
+                                errored = true;
 
-                                    vec![SpannedValue(Value::Error, f.clone().1)]
-                                }
+                                SpannedValue(Value::Error, f.clone().1)
                             }
-                        })
-                        .flatten()
-                        .collect();
+                        }
+                    })
+                    .collect();
 
-                    if errored {
-                        return Err(errors);
-                    } else {
-                        Ok(SpannedValue(Value::Array(new), span.clone()))
-                    }
-                }
-                _ => Ok(SpannedValue(Value::from(literal.clone()), span.clone())),
-            },
-            Spanned(Expr::InfixOp(lhs, op, rhs), span) => {
-                let lhs = interpret(*lhs);
-                let lhs = match lhs {
-                    Err(e) => {
-                        errors.extend(e);
-
-                        SpannedValue(Value::Error, 0..1)
-                    }
-                    Ok(e) => e[0].clone(),
-                };
-
-                let rhs = interpret(*rhs);
-                let rhs = match rhs {
-                    Err(e) => {
-                        errors.extend(e);
-
-                        SpannedValue(Value::Error, 0..1)
-                    }
-                    Ok(e) => e[0].clone(),
-                };
-
-                if lhs == Value::Error || rhs == Value::Error {
-                    return Err(errors);
-                }
-
-                let output = match op {
-                    InfixOp::Pow => lhs.pow(rhs),
-                    InfixOp::Mul => lhs.mul(rhs),
-                    InfixOp::Div => lhs.div(rhs),
-                    InfixOp::Mod => lhs.modulus(rhs),
-                    InfixOp::Add => lhs.add(rhs),
-                    InfixOp::Sub => lhs.sub(rhs),
-                    InfixOp::Equals => lhs.equals(rhs),
-                    InfixOp::NotEquals => lhs.equals(rhs),
-                    InfixOp::Lt => lhs.lt(rhs),
-                    InfixOp::Gt => lhs.gt(rhs),
-                    InfixOp::Lte => lhs.lte(rhs),
-                    InfixOp::Gte => lhs.gte(rhs),
-                    InfixOp::And => lhs.and(rhs),
-                    InfixOp::Or => lhs.or(rhs),
-                    InfixOp::In => lhs.is_in(rhs),
-                };
-
-                match output {
-                    Err(e) => {
-                        errors.push(e);
-
-                        return Err(errors);
-                    }
-                    Ok(e) => Ok(SpannedValue(e, span.clone())),
+                if errored {
+                    Err(errors)
+                } else {
+                    Ok(SpannedValue(Value::Array(new), span.clone()))
                 }
             }
-            Spanned(Expr::Not(rhs), span) => {
-                let rhs = interpret(*rhs);
-                let rhs = match rhs {
-                    Err(e) => {
-                        errors.extend(e);
+            _ => Ok(SpannedValue(Value::from(literal.clone()), span.clone())),
+        },
+        Spanned(Expr::InfixOp(lhs, op, rhs), span) => {
+            let lhs = interpret(lhs);
+            let lhs = match lhs {
+                Err(e) => {
+                    errors.extend(e);
 
-                        return Err(errors);
-                    }
-                    Ok(e) => e[0].clone(),
-                };
-
-                let output = rhs.not();
-
-                match output {
-                    Err(e) => {
-                        errors.push(e);
-
-                        return Err(errors);
-                    }
-                    Ok(e) => Ok(SpannedValue(e, span.clone())),
+                    SpannedValue(Value::Error, 0..1)
                 }
+                Ok(e) => e.clone(),
+            };
+
+            let rhs = interpret(rhs);
+            let rhs = match rhs {
+                Err(e) => {
+                    errors.extend(e);
+
+                    SpannedValue(Value::Error, 0..1)
+                }
+                Ok(e) => e.clone(),
+            };
+
+            if lhs == Value::Error || rhs == Value::Error {
+                return Err(errors);
             }
-            Spanned(Expr::Index(lhs, rhs), span) => {
-                let lhs = interpret(*lhs);
-                let lhs = match lhs {
-                    Err(e) => {
-                        errors.extend(e);
 
-                        SpannedValue(Value::Error, 0..1)
-                    }
-                    Ok(e) => e[0].clone(),
-                };
+            let output = match op {
+                InfixOp::Pow => lhs.pow(rhs),
+                InfixOp::Mul => lhs.mul(rhs),
+                InfixOp::Div => lhs.div(rhs),
+                InfixOp::Mod => lhs.modulus(rhs),
+                InfixOp::Add => lhs.add(rhs),
+                InfixOp::Sub => lhs.sub(rhs),
+                InfixOp::Equals => lhs.equals(rhs),
+                InfixOp::NotEquals => lhs.equals(rhs),
+                InfixOp::Lt => lhs.lt(rhs),
+                InfixOp::Gt => lhs.gt(rhs),
+                InfixOp::Lte => lhs.lte(rhs),
+                InfixOp::Gte => lhs.gte(rhs),
+                InfixOp::And => lhs.and(rhs),
+                InfixOp::Or => lhs.or(rhs),
+                InfixOp::In => lhs.is_in(rhs),
+            };
 
-                let rhs = interpret(*rhs);
-                let rhs = match rhs {
-                    Err(e) => {
-                        errors.extend(e);
+            match output {
+                Err(e) => {
+                    errors.push(e);
 
-                        SpannedValue(Value::Error, 0..1)
-                    }
-                    Ok(e) => e[0].clone(),
-                };
-
-                if lhs == Value::Error || rhs == Value::Error {
-                    return Err(errors);
+                    Err(errors)
                 }
-
-                let output = lhs.index(rhs);
-
-                match output {
-                    Err(e) => {
-                        errors.push(e);
-
-                        return Err(errors);
-                    }
-                    Ok(e) => Ok(SpannedValue(e, span.clone())),
-                }
+                Ok(e) => Ok(SpannedValue(e, span.clone())),
             }
-            _ => todo!(),
-        };
-
-        match out {
-            Ok(e) => results.push(e),
-            Err(e) => return Err(e),
         }
-    }
+        Spanned(Expr::Not(rhs), span) => {
+            let rhs = interpret(rhs);
+            let rhs = match rhs {
+                Err(e) => {
+                    errors.extend(e);
 
-    Ok(results)
+                    return Err(errors);
+                }
+                Ok(e) => e.clone(),
+            };
+
+            let output = rhs.not();
+
+            match output {
+                Err(e) => {
+                    errors.push(e);
+
+                    Err(errors)
+                }
+                Ok(e) => Ok(SpannedValue(e, span.clone())),
+            }
+        }
+        Spanned(Expr::Index(lhs, rhs), span) => {
+            let lhs = interpret(lhs);
+            let lhs = match lhs {
+                Err(e) => {
+                    errors.extend(e);
+
+                    SpannedValue(Value::Error, 0..1)
+                }
+                Ok(e) => e.clone(),
+            };
+
+            let rhs = interpret(rhs);
+            let rhs = match rhs {
+                Err(e) => {
+                    errors.extend(e);
+
+                    SpannedValue(Value::Error, 0..1)
+                }
+                Ok(e) => e.clone(),
+            };
+
+            if lhs == Value::Error || rhs == Value::Error {
+                return Err(errors);
+            }
+
+            let output = lhs.index(rhs);
+
+            match output {
+                Err(e) => {
+                    errors.push(e);
+
+                    Err(errors)
+                }
+                Ok(e) => Ok(SpannedValue(e, span.clone())),
+            }
+        }
+        _ => todo!(),
+    }
 }
 
 #[cfg(test)]
@@ -277,55 +266,55 @@ mod tests {
 
     #[test]
     fn interpret_num() {
-        let parsed = parse("12");
+        let parsed = &parse("12")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(12.0)])
+        assert_eq!(interpreted, Value::Num(12.0))
     }
 
     #[test]
     fn interpret_addition() {
-        let parsed = parse("12 + 8");
+        let parsed = &parse("12 + 8")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(20.0)])
+        assert_eq!(interpreted, Value::Num(20.0))
     }
 
     #[test]
     fn interpret_chained() {
-        let parsed = parse("12 + 8 * 3");
+        let parsed = &parse("12 + 8 * 3")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(36.0)])
+        assert_eq!(interpreted, Value::Num(36.0))
     }
 
     #[test]
     fn interpret_parens() {
-        let parsed = parse("(12 + 8) / 10");
+        let parsed = &parse("(12 + 8) / 10")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(2.0)])
+        assert_eq!(interpreted, Value::Num(2.0))
     }
 
     #[test]
     fn interpret_complex_chained() {
-        let parsed = parse("10 + (30 - 5) * 3 ** 2");
+        let parsed = &parse("10 + (30 - 5) * 3 ** 2")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(235.0)])
+        assert_eq!(interpreted, Value::Num(235.0))
     }
 
     #[test]
     fn interpret_string_mul() {
-        let parsed = parse("'nice' * 3");
+        let parsed = &parse("'nice' * 3")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::String("nicenicenice".to_owned())])
+        assert_eq!(interpreted, Value::String("nicenicenice".to_owned()))
     }
 
     #[test]
     fn interpret_string_mul_invalid_direct() {
-        let parsed = parse("'nice' * 'cool'");
+        let parsed = &parse("'nice' * 'cool'")[0];
         let interpreted = interpret(parsed);
 
         assert!(interpreted.is_err())
@@ -333,7 +322,7 @@ mod tests {
 
     #[test]
     fn interpret_string_mul_invalid_chain() {
-        let parsed = parse("'nice' * (3 * 'cool')");
+        let parsed = &parse("'nice' * (3 * 'cool')")[0];
         let interpreted = interpret(parsed);
 
         assert!(interpreted.is_err())
@@ -341,57 +330,57 @@ mod tests {
 
     #[test]
     fn interpret_index_array() {
-        let parsed = parse("[1, 2, 3, 4][3]");
+        let parsed = &parse("[1, 2, 3, 4][3]")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Num(4.0)])
+        assert_eq!(interpreted, Value::Num(4.0))
     }
 
     #[test]
     fn interpret_index_string() {
-        let parsed = parse("'nice'[3]");
+        let parsed = &parse("'nice'[3]")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::String("e".to_owned())])
+        assert_eq!(interpreted, Value::String("e".to_owned()))
     }
 
     #[test]
     fn interpret_and_true() {
-        let parsed = parse("true and true");
+        let parsed = &parse("true and true")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Bool(true)])
+        assert_eq!(interpreted, Value::Bool(true))
     }
 
     #[test]
     fn interpret_and_false() {
-        let parsed = parse("true and false");
+        let parsed = &parse("true and false")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Bool(false)])
+        assert_eq!(interpreted, Value::Bool(false))
     }
 
     #[test]
     fn interpret_or_true() {
-        let parsed = parse("true or false");
+        let parsed = &parse("true or false")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Bool(true)])
+        assert_eq!(interpreted, Value::Bool(true))
     }
 
     #[test]
     fn interpret_or_false() {
-        let parsed = parse("false or false");
+        let parsed = &parse("false or false")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Bool(false)])
+        assert_eq!(interpreted, Value::Bool(false))
     }
 
     #[test]
     fn interpret_chain_and_or() {
-        let parsed = parse("true and false or true and true");
+        let parsed = &parse("true and false or true and true")[0];
         let interpreted = interpret(parsed).unwrap();
 
-        assert_eq!(interpreted, vec![Value::Bool(true)])
+        assert_eq!(interpreted, Value::Bool(true))
     }
 }
